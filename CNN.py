7b89f41d585f0b4c730babfad154c9e52ed4faad
nivkrig <86209884+nivkrig@users.noreply.github.com>
2023-12-12 17:05:39 -0500

    Update and rename CNN.md to CNN.py

--- CNN.py
+++ CNN.py
@@ -1,12 +1,10 @@
 ### main document for training and saving the model 
-```
+
 import tensorflow as tf
-```
-```
+
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
-```
-```
+
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
@@ -17,20 +15,17 @@ import numpy as np
 import cv2
 import json
 import h5py
-```
-```
+
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
-```
-```
+
 num_filters = 60
 size_filter1 = (5, 5)
 size_filter2 = (3, 3)
 size_pool = (2, 2)
 num_node = 500
-```
-```
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(num_filters, size_filter1, activation="relu", input_shape=(28, 28, 1)),
     tf.keras.layers.Conv2D(num_filters, size_filter1, activation="relu"),
@@ -46,29 +41,24 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation="softmax")
 
 ])
-```
-```
+
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-```
-```
+
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-```
-```
+
 history = model.fit(x_train, y_train, epochs=6)
 model.evaluate(x_test,  y_test, verbose=2)
-```
+
 
 #### Model saves to a .py file ( try save .h5) (, save_format = "h5") or tf
 
 
-```
-model.save('model')
-```
 
-```
+model.save('model')
+
 cv2.imshow('test', x_train[0])
 cv2.waitKey()
 
